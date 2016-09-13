6c9f71cac3486d1398331388a96a1ac5553f6632
Adithya <ra102@duke.edu>
2016-09-13 16:56:25 -0400

    Large image animation test

--- src/Triangle.java
+++ src/Triangle.java
@@ -13,15 +13,23 @@ import com.jogamp.opengl.GLEventListener;
 import com.jogamp.opengl.util.texture.*;
 import com.jogamp.opengl.util.texture.awt.AWTTextureIO;
 
+import com.jogamp.opengl.util.Animator;
 
 public class Triangle implements GLEventListener {
 	private Texture earthTexture;
+	
+	private Animator a;
+	private long startTime;
+	
 	@Override
 	public void display(GLAutoDrawable drawable) {
-        render(drawable);
+        //render(drawable);
+        render(drawable, System.currentTimeMillis());
+		//System.out.println("Is animator running? " + a.isAnimating());
+        System.out.println(a.getLastFPS());
 		
 	}
-	private void render(GLAutoDrawable drawable) {
+	private void render(GLAutoDrawable drawable, long time) {
 		GL2 gl = drawable.getGL().getGL2();
         gl.glClear(GL.GL_COLOR_BUFFER_BIT);
         gl.glEnable(GL.GL_TEXTURE_2D);
@@ -32,11 +40,11 @@ public class Triangle implements GLEventListener {
       
 
 //	    gl.glColor3f( 1.0f, 0.0f, 0.0f);                     // Set colour to red
-	    gl.glTexCoord2f(0.0f, 0.0f);
-	    gl.glVertex3f( 0.0f, 1.0f, 1.0f);                       // Top vertex
-	    gl.glTexCoord2f(1.0f, 1.0f);
+	    gl.glTexCoord2f(0.0f + (float)(time - startTime)/5000, 0.0f);
+	    gl.glVertex3f( 0.0f, 0.0f, 0.0f);                       // Top vertex
+	    gl.glTexCoord2f(1.0f + (float)(time - startTime)/5000, 1.0f);
 	    gl.glVertex3f(-1.0f,-1.0f, 0.0f);                       // Bottom left vertex
-	    gl.glTexCoord2f(0.0f, 1.0f);
+	    gl.glTexCoord2f(0.0f + (float)(time - startTime)/5000, 1.0f);
 	    gl.glVertex3f( 1.0f,-1.0f, 0.0f);                       // Bottom right vertex
 
         gl.glEnd();
@@ -71,12 +79,18 @@ public class Triangle implements GLEventListener {
 	public void init(GLAutoDrawable arg0) {
 		GL2 gl = arg0.getGL().getGL2();
 		try {
-			File f = new File("testimage1.JPG"); 
+			File f = new File("lib/LargeImage.jpg"); 
 			boolean exists = f.exists(); 
-			BufferedImage bufferedImage = ImageIO.read(f);
 			System.out.println(exists);
+			BufferedImage bufferedImage = ImageIO.read(f);
+			System.out.println("Buffered Image successfully created");
+
 //			InputStream stream = new BufferedInputStream(new FileInputStream("testimage1.JPG"));
 			earthTexture = AWTTextureIO.newTexture(gl.getGLProfile(), bufferedImage, true);
+			a = new Animator(arg0);
+			a.start();
+			startTime = System.currentTimeMillis();
+		
 		}
 		catch (IOException exc) {
 			exc.printStackTrace();
